156363d096013a5ab853511547af1fb116628ad8
iagofontes <iagofontes@hotmail.com>
2020-03-30 14:44:17 -0300

    inicio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Calculadora from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './main/Calculator';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator/>
+  </div>,
   document.getElementById('root')
 );
 
